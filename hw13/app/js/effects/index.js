555effa60f0b2ad4e277bf1f13c17c7cf19c98ef
DzividzinskaM <marianadzividzinska04@gmail.com>
2020-05-13 22:19:35 +0300

    change task 2

--- hw13/app/js/effects/index.js
+++ hw13/app/js/effects/index.js
@@ -1,18 +1,19 @@
 import { TodosEffects } from "./todos.effects";
-
+import {tryToGetTodos} from "../constants/action"
 function useEffect(actionType, todosEffects) {
   window.store.dispatch({type: actionType});
 
-  if(window.store.getState().todos.isLoading){
-    console.log("Loading....");
-    setTimeout(()=>{
-      todosEffects.getTodos();
-    }, 300);
-    
-  } else{
-    console.log("Error");
-  } 
-  
+  if(actionType == tryToGetTodos){
+    if(window.store.getState().todos.isLoading){
+      console.log("Loading....");
+      setTimeout(()=>{
+        todosEffects.getTodos();
+      }, 300);
+      
+    } else{
+      console.log("Error");
+    } 
+  }
 }
 
 export { TodosEffects, useEffect };
